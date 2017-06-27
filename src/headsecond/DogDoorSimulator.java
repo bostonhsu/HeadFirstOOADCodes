b11b874614c6832e6737a8fa615780721587699f
Boston Hsu <bostonhsu@outlook.com>
2017-06-27 11:18:14 +0800

    完成测试驱动程序。

---
+++ src/headsecond/DogDoorSimulator.java
@@ -0,0 +1,19 @@
+package headsecond;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class DogDoorSimulator {
+    public static void main(String[] args) {
+        DogDoor dogDoor = new DogDoor ();
+        Remote remote = new Remote (dogDoor);
+        System.out.println ("Fido barks to go outsides...");
+        remote.pressButton ();
+        System.out.println ("Fido has gone outside...");
+        remote.pressButton ();
+        System.out.println ("Fido's all done...");
+        remote.pressButton ();
+        System.out.println ("Fido's back inside...");
+        remote.pressButton ();
+    }
+}
