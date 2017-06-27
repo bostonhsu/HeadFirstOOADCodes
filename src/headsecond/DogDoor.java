d4b7e6dcdf4541491dc7b299b921a50491e2d662
Boston Hsu <bostonhsu@outlook.com>
2017-06-27 11:15:51 +0800

    第二章开始。

---
+++ src/headsecond/DogDoor.java
@@ -0,0 +1,26 @@
+package headsecond;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class DogDoor {
+    private boolean open;
+
+    public DogDoor() {
+        this.open = false;
+    }
+
+    public void open() {
+        System.out.println ("The dog door opens.");
+        open = true;
+    }
+
+    public void close() {
+        System.out.println ("The dog door closes.");
+        open = false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+}
---
+++ src/headsecond/Remote.java
@@ -0,0 +1,21 @@
+package headsecond;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+public class Remote {
+    private DogDoor dogDoor;
+
+    public Remote(DogDoor dogDoor) {
+        this.dogDoor = dogDoor;
+    }
+
+    public void pressButton() {
+        System.out.println ("Pressing the remote control button...");
+        if (dogDoor.isOpen ()) {
+            dogDoor.open ();
+        } else {
+            dogDoor.close ();
+        }
+    }
+}
