0cbdb59f93e4471da7ece3d818282c1e44477ea6
Boston Hsu <bostonhsu@outlook.com>
2017-07-04 11:49:22 +0800

    Randy版的DogDoor。他的宗旨是简单就是美。

--- src/headsecond/DogDoor.java
+++ src/headsecond/DogDoor.java
@@ -8,9 +8,18 @@ import java.util.TimerTask;
  */
 public class DogDoor {
     private boolean open;
+    private String allowedBark;
 
     public DogDoor() {
         this.open = false;
+    }
+
+    public void setAllowedBark(String bark) {
+        allowedBark = bark;
+    }
+
+    public String getAllowedBark() {
+        return allowedBark;
     }
 
     public void open() {
